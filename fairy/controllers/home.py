8a76111d821f5c7a7b81aed3167615173b2d64de
cj <604669163@qq.com>
2016-07-08 09:00:03 +0800

    add:database.sql.

--- fairy/controllers/home.py
+++ fairy/controllers/home.py
@@ -1,9 +1,12 @@
 # config=utf-8
 from flask import Blueprint, render_template
+from fairy.model.books import get_book_list
 
 homeRoute = Blueprint('home', __name__, url_prefix='/', template_folder='templates')
 
 
 @homeRoute.route('/')
 def index():
-    return render_template('home/index.html')
+    items = get_book_list(1, 10)
+
+    return render_template('home/index.html', items=items)
