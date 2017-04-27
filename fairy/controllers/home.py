e4267205c50f2c5dbc283dc01c32874b015dc7ba
cj <604669163@qq.com>
2017-04-27 16:14:48 +0800

    增加：去除页面缓存变量。

--- fairy/controllers/home.py
+++ fairy/controllers/home.py
@@ -1,4 +1,5 @@
 # config=utf-8
+import time
 from flask import Blueprint, render_template
 from fairy.model.books import get_book_list
 
@@ -9,5 +10,9 @@ homeRoute = Blueprint('home', __name__, url_prefix='/', template_folder='templat
 def index():
     recommend_items = get_book_list(1, 12, 2)
     new_items = get_book_list(1, 24)
+    now_format = time.time()
 
-    return render_template('home/index.html', recommend_items=recommend_items, new_items=new_items)
+    return render_template('home/index.html',
+                           recommend_items=recommend_items,
+                           new_items=new_items,
+                           now_format=now_format)
